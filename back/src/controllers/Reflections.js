5860e2bac13186147c46e9b623eaafac9bfa3623
Eduard <eduard.dubrivski@gmail.com>
2019-01-06 12:18:36 +0000

    dollar

--- back/src/controllers/Reflections.js
+++ back/src/controllers/Reflections.js
@@ -8,7 +8,7 @@ const Reflection = {
 	* @returns {object} reflection object
 	*/
 	create(req, res) {
-		if (!req.body.success $$ !req.body.lowPoint $$ !req.body.takeAway) {
+		if (!req.body.success && !req.body.lowPoint && !req.body.takeAway) {
 			return res.status(400).send({'message':'All fields are required'})
 		}
 		const reflection = ReflectionModel.create(req.body);
