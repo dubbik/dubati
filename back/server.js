4f8a7924808cdbca75dd68dd1330134270711cca
Eduard <eduard.dubrivski@gmail.com>
2019-01-09 01:54:28 +0000

    now node js is connected to database and running script adds new dynamic value to the table

--- back/server.js
+++ back/server.js
@@ -4,22 +4,33 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { Pool, Client } = require('pg')
 
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'postgres',
+  port: 5432,
+});
+
+var now = new Date().toISOString();
+var insert = "INSERT INTO messages VALUES ('AI-2', 'Now is: "+now+"', '"+now+"')";
+//console.log(insert);
+pool.query(insert, (err, res) => {
+	console.log(err);
+});
+//pool.query("INSERT INTO messages VALUES ('Alina', 'One more message 3', '2019-01-07 02:03:05')", (err, res) => {});
+pool.query("SELECT * FROM messages", (err, res) => {
+//	var objLen = Object.keys(res.rows).length;
+	console.log(res.rows);
+	pool.end();
+});
+
+//Project 1
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 app.use(express.json());
-
-app.get('/', (req, res) => {
-	return res.status(200).send({'message': 'This is the main script'});});
-
-const Reflection = require('./src/controllers/Reflections');
-
-app.post('api/v1/reflections', Reflection.create);
-app.get('api/v1/reflections', Reflection.getAll);
-app.get('api/v1/reflections', Reflection.getOne);
-app.put('api/v1/reflections', Reflection.update);
-app.delete('api/v1/reflections', Reflection.delete);
-
 app.get('/api/', (request, response) => {
     var number = request.query.number;
     if(number == 666){
